21aebf3a777d634018e3513198fe383266f9130a
jacoobes <jngu195@lsu.edu>
2024-08-01 23:13:25 -0500

    d1p1

--- src/advent_of_code/day_01.clj
+++ src/advent_of_code/day_01.clj
@@ -1,9 +1,14 @@
-(ns advent-of-code.day-01)
+(ns advent-of-code.day-01
+  (:require [clojure.string :as str]))
 
 (defn part-1
   "Day 01 Part 1"
   [input]
-  input)
+  (->> (str/split input #"\r?\n")
+       (partition-by str/blank?)
+       (map #(reduce + (map clojure.edn/read-string %)))
+       (filter some?)
+       (apply max)))
 
 (defn part-2
   "Day 01 Part 2"
