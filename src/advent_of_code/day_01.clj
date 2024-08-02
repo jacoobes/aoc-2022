f896e95ae4e062193cd30abef315f1ebd3a8e753
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-08-01 23:19:52 -0500

    d1-complete

--- src/advent_of_code/day_01.clj
+++ src/advent_of_code/day_01.clj
@@ -1,16 +1,17 @@
 (ns advent-of-code.day-01
   (:require [clojure.string :as str]))
 
-(defn part-1
-  "Day 01 Part 1"
-  [input]
+
+(defn get-nums [input]
   (->> (str/split input #"\r?\n")
        (partition-by str/blank?)
        (map #(reduce + (map clojure.edn/read-string %)))
-       (filter some?)
-       (apply max)))
+       (filter some?)))
+(defn part-1
+  "Day 01 Part 1"
+  [input]
+  (apply (get-nums input) max))
 
 (defn part-2
   "Day 01 Part 2"
-  [input]
-  input)
+  [input] (apply + (take 3 (sort > (get-nums input)))))
