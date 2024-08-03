8fb2738fa283d83fc1b676a8800acd929aa20834
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-08-03 17:09:29 -0500

    p

--- src/advent_of_code/day_05.clj
+++ src/advent_of_code/day_05.clj
@@ -1,9 +1,44 @@
-(ns advent-of-code.day-05)
+(ns advent-of-code.day-05
+  (:require [clojure.string :as str])
+  (:require [clojure.pprint :refer [ pprint]]))
+
+(defn instructions [lines]
+  ; mv x from y to z
+  (->> (map #(re-seq #"\d" %) lines)
+       (map #(map read-string %))))
+
+(defn parse-line [numcol line]
+   (let [len (count line)]
+    (->> (map (partial get line) (range 1 len 4)))))
+
+(defn parse-chart [lines]
+  (let [[numcol & data] lines
+        num-stacks (count (re-seq #"\S+" numcol))]
+        ;transpose matrix
+        (->> (apply mapv vector (map (partial parse-line num-stacks) data))
+             (map (fn [r] (filter #(not= \space %) r)))
+             (map vec))))
+
+
+(defn parse [lines]
+   (loop [[fst nxt & rst] lines
+           chart []] 
+     (if (str/starts-with? nxt "move")
+          {:chart (vec (parse-chart (reverse chart)))  
+           :instructions (instructions (cons nxt rst))}  
+          (recur (cons nxt rst) (conj chart fst)))))
+
+(defn transaction [quant from acc dest]
+  dest
+   )
 
 (defn part-1
   "Day 05 Part 1"
   [input]
-  input)
+  (let [{:keys [chart instructions]} (parse (str/split-lines input)) ]
+    (reduce (fn [acc [quant from dest]]
+              (update acc (dec dest) (partial transaction quant from acc))) 
+            chart instructions )))
 
 (defn part-2
   "Day 05 Part 2"
