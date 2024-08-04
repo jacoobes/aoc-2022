c176121923de4835a0dce37c4e1ebaa90d897ace
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-08-04 13:41:20 -0500

    day6

--- src/advent_of_code/day_06.clj
+++ src/advent_of_code/day_06.clj
@@ -1,11 +1,15 @@
 (ns advent-of-code.day-06)
 
+(defn decode-stream [st mark]
+  (loop [[[i ch] & rst] (map-indexed vector st) ]
+    (if (= mark (count (distinct (cons ch (map second (take (dec mark) rst))))))
+      (+ mark i) 
+      (recur rst))))
+
 (defn part-1
   "Day 06 Part 1"
-  [input]
-  input)
+  [input] (decode-stream input 4))
 
 (defn part-2
   "Day 06 Part 2"
-  [input]
-  input)
+  [input] (decode-stream input 14))
