80b5fcb410dd0bfb00b46f923d2d434dfb3c6423
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-08-04 16:35:23 -0500

    progress on d7

--- src/advent_of_code/day_07.clj
+++ src/advent_of_code/day_07.clj
@@ -1,9 +1,43 @@
-(ns advent-of-code.day-07)
+(ns advent-of-code.day-07
+  (:require [clojure.string :as str]))
 
+(defn usr-command [s] 
+  (let [[_ command arg] (str/split s #" +")]
+    { :command command :arg arg}))
+
+(defn dirent [s]
+  (let [[command arg] (str/split s #" +")]
+    (if (= command "dir")
+        {:size 0 :name arg}
+        {:size (Long/parseLong command) :name arg })))
+
+(defn into-tree [_lines]
+  (loop [[fst & rst] (next _lines) 
+         dirpath ["/"]
+         tree {}]
+    (if (zero? (count rst))
+        tree
+        (cond 
+           (= "ls" (fst :command)) (let [entries (vec (take-while #(some? (:size %)) rst)) 
+                                         centries (count entries)]
+                                     (recur (drop centries rst) dirpath
+                                            (update-in tree dirpath (fn [dest]
+                                                                          {:entries entries 
+                                                                           :parent (->> dirpath pop peek)} ))))
+           (= "cd" (fst :command)) (let [dest (:arg fst)]
+                                      (if (= ".." dest)
+                                        (recur rst (pop dirpath) tree)
+                                        (let [curdir (peek dirpath)]
+                                          (recur rst (conj dirpath dest) tree))))))))
+(defn parse-output [lines] 
+  (let [tree (->> lines (map #(if (str/starts-with? % "$") (usr-command %) (dirent %)))
+                        into-tree)]
+    ))
 (defn part-1
   "Day 07 Part 1"
   [input]
-  input)
+  (->> (clojure.string/split-lines input)
+       (parse-output)))
 
 (defn part-2
   "Day 07 Part 2"
