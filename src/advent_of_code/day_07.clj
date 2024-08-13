63715847679b62d1cb90539e0c3b58eb205df382
jacoobes <jngu195@lsu.edu>
2024-08-13 17:02:07 -0500

    sum

--- src/advent_of_code/day_07.clj
+++ src/advent_of_code/day_07.clj
@@ -21,9 +21,8 @@
            (= "ls" (fst :command)) (let [entries (vec (take-while #(some? (:size %)) rst)) 
                                          centries (count entries)]
                                      (recur (drop centries rst) dirpath
-                                            (update-in tree dirpath (fn [dest]
-                                                                          {:entries entries 
-                                                                           :parent (->> dirpath pop peek)} ))))
+                                            (update-in tree dirpath (fn [dest] {:size (reduce + (map :size entries)) 
+                                                                                      :parent (->> dirpath pop peek)} ))))
            (= "cd" (fst :command)) (let [dest (:arg fst)]
                                       (if (= ".." dest)
                                         (recur rst (pop dirpath) tree)
@@ -31,13 +30,18 @@
                                           (recur rst (conj dirpath dest) tree))))))))
 (defn parse-output [lines] 
   (let [tree (->> lines (map #(if (str/starts-with? % "$") (usr-command %) (dirent %)))
-                        into-tree)]
-    ))
+                        into-tree)] 
+     (tree-seq map? identity tree)))
 (defn part-1
   "Day 07 Part 1"
   [input]
   (->> (clojure.string/split-lines input)
        (parse-output)))
+
+(let [input (slurp "./resources/day-07-example.txt")]
+  (part-1 input))
+
+(reduce (fn [acc [k v]] (println k)) {:a 1 :b 5})
 
 (defn part-2
   "Day 07 Part 2"
